f91186a24dcbcfde47e63fbc040a55168b9c8177
Agustin <alphamikevictor@gmail.com>
2016-03-01 20:41:19 +0100

    Primer commit requete cutre

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>Things to do</title>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.9/angular.min.js"></script>
+<link rel=s"tylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.css">
+<head>
+<body>
+<table class="table">
+<tr><td>Usuario</td><td>eMail</td></tr>
+<?php
+	$conn = new mysqli(getenv("MYSQL_HOST"),getenv("MYSQL_USER"),getenv("MYSQL_PASSWORD")i,getenv("MYSQL_DB"));
+	$SQL  = "select * from users";
+	$results = $conn->query($SQL);
+	while ($user = $results->fetch_assoc()){
+	?>
+	<tr><td><?php echo $user['username']?></td><td><?php echo $user['email']?></td></tr>
+	<?php
+	}
+
+?>
+</body>
+</html>
